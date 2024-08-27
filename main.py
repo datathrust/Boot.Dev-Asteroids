b5c60d04bc40b849970f3eee055b5061e4cb384f
DataThrust <shannan@datathrust.net>
2024-08-27 15:47:21 +0930

    Updated main; basic primary game loop.

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
+from constants import *
 
 def main():
-    print(f"Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_status = True
+
+    while game_status:
+        # Respond to window closure
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        #Setup basic background:
+        screen.fill('#000000')
+        pygame.display.flip()
+        
+    # print(f"Starting asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == '__main__': 
     main()
